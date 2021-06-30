f272bdc4d14fa5d1f452d0b8d6af34e8b9c43107
Jon Kelly <jon.kelly@twistedtechnology.co.uk>
2021-06-30 20:58:39 +0100

    Initial commit

---
+++ components/Post.js
@@ -0,0 +1,30 @@
+import Link from 'next/Link';
+// import Image from 'next/image';
+
+function Post({ post }) {
+    return (
+        <div className='card'>
+
+            {/*}
+            <Image
+                src={post.frontmatter.cover_image}
+                alt={post.slug.replace('-', ' ') + ' title image'}
+                width="512px"
+                height="256px"
+            />
+            */}
+            <img src={post.frontmatter.cover_image} alt={post.slug.replace('-', ' ') + ' title image'} />
+            <div className="post-date">Posted on {post.frontmatter.date}</div>
+            <h3>{post.frontmatter.title}</h3>
+            <p>{post.frontmatter.export}</p>
+
+            <Link href={`/blog/${post.slug}`} passHref>
+                <a href="" className="btn">
+                    Read More
+                </a>
+            </Link>
+        </div>
+    );
+};
+
+export default Post;
